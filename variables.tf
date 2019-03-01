bff6163bf4b45328a5e453cd13941b715766cafb
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2019-03-01 11:11:01 +0000

    Add the ability to create tcp lb

--- variables.tf
+++ variables.tf
@@ -46,12 +46,6 @@ variable "hostnames" {
   type = "list"
   default = []
 }
-
-variable "test" {
-  description = "The list of alias DNS"
-  type = "list"
-  default = []
-}
 variable "protocols" {
   description = "The list of protocols to use for each hostname"
   type = "list"
